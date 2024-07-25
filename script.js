4004f917fc51ff2d499f6e977f6546541c6c05ce
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2024-07-25 13:38:56 -0400

    setting up event listeners  for the buttons and future functions

---
+++ script.js
@@ -0,0 +1,28 @@
+const time = document.querySelector(".time");
+const startButton = document.querySelector(".start-button");
+const stopButton = document.querySelector(".stop-button");
+const resetButton = document.querySelector(".reset-button");
+
+startButton.addEventListener("click", () => {
+    startClock();
+})
+
+stopButton.addEventListener("click", () => {
+    stopClock();
+})
+
+resetButton.addEventListener("click", () => {
+    resetClock();
+})
+
+function startClock(){
+
+}
+
+function stopClock(){
+    
+}
+
+function resetClock(){
+    
+}
